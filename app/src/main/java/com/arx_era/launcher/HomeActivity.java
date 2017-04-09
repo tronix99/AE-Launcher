bd8ea51919dd9a60084eee2f52393530761b7e6d
Tronix <rajdevkar.6@gmail.com>
2017-04-09 19:02:41 +0530

    Second commit

---
+++ app/src/main/java/com/arx_era/launcher/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.arx_era.launcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+
+        LinearLayout touch = (LinearLayout) findViewById(R.id.touch_area);
+        touch.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getApplicationContext(),"Hello", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+}
