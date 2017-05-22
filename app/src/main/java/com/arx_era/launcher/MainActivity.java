bb416364ff3b75567348e7e63b0ac36c1800e72f
tronix99 <rajdevkar.6@gmail.com>
2017-05-22 21:22:43 +0530

    IN-PROGRESS

--- app/src/main/java/com/arx_era/launcher/MainActivity.java
+++ app/src/main/java/com/arx_era/launcher/MainActivity.java
@@ -25,8 +25,6 @@ public class MainActivity extends FragmentActivity {
 
         viewPager.setAdapter(mAdapter);
         viewPager.setCurrentItem(1);
-
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     @Override
