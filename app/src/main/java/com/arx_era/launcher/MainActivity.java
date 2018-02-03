b39b083f89a561eee3f854d15af71b49694c08ca
tronix99 <rajdevkar.6@gmail.com>
2018-02-03 20:55:47 +0530

    Update

--- app/src/main/java/com/arx_era/launcher/MainActivity.java
+++ app/src/main/java/com/arx_era/launcher/MainActivity.java
@@ -1,16 +1,29 @@
 package com.arx_era.launcher;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-public class MainActivity extends FragmentActivity {
+import com.arx_era.WallpaperChooser.WallpaperChooser;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
 
-    }
+        View home = findViewById(R.id.HomeLayout);
+        home.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WallpaperChooser.class);
+                startActivity(Intent.createChooser(intent, "Select Wallpaper"));
+                return false;
+            }
+        });
+   }
 
     @Override
     public void onBackPressed() {
