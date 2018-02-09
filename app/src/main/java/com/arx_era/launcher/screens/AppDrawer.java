f81eced5b3a8c2e5f1d598dfbb3346a4c9fdc63d
tronix99 <rajdevkar.6@gmail.com>
2018-02-09 22:53:34 +0530

    Update

---
+++ app/src/main/java/com/arx_era/launcher/screens/AppDrawer.java
@@ -0,0 +1,44 @@
+package com.arx_era.launcher.screens;
+
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arx_era.launcher.BlurDrawable;
+import com.arx_era.launcher.R;
+
+/**
+ * Created by tronix99 on 9/2/18.
+ */
+
+public class AppDrawer extends Fragment{
+
+    public AppDrawer() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.app_drawer, container, false);
+
+        return v;
+    }
+
+}
